336f88ae40a5c738fa80ecabf061e1b48cd4ea2c
nurxon <nafasovnurxon18@gmail.com>
2024-02-12 20:28:52 +0500

    modified

--- apps/contact/urls.py
+++ apps/contact/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import ContactView
+from .views import ContactView, LoginView, LogoutView, RegisterView
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('contact/', ContactView.as_view(), name='contact')
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
 
 ]
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -5,3 +5,15 @@ from django.views.generic import TemplateView
 
 class ContactView(TemplateView):
     template_name = 'contact/contact.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'contact/login.html'
+
+
+class RegisterView(TemplateView):
+    template_name = 'contact/registration.html'
+
+
+class LogoutView(TemplateView):
+    template_name = 'contact/logout.html'
